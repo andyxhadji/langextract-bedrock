e35824b611718956dba42afaa3e3bee7f574ecca
lapiceroazul4 <lapiceroazul@proton.me>
2026-01-13 22:38:17 -0500

    fix & improvements :)

--- langextract_bedrock/__init__.py
+++ langextract_bedrock/__init__.py
@@ -1,6 +1,7 @@
 """LangExtract provider plugin for Bedrock."""
 
 from langextract_bedrock.provider import BedrockLanguageModel
+from langextract_bedrock.schema import BedrockToolUseSchema
 
-__all__ = ['BedrockLanguageModel']
-__version__ = "0.1.0"
+__all__ = ["BedrockLanguageModel", "BedrockToolUseSchema"]
+__version__ = "0.1.5"
